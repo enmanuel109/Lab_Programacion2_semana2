3e4e4c209c87064547c64fb9012fe5cd2fc0061b
Marcio Alberto Vasquez Cordova <mavasquez@cemcol.com>
2025-10-24 09:31:35 -0600

    Clase EmpleadoVenta terminada

--- Lab_Programacion2_semana2/src/lab_programacion2_semana2/EmpleadoVentas.java
+++ Lab_Programacion2_semana2/src/lab_programacion2_semana2/EmpleadoVentas.java
@@ -4,10 +4,45 @@
  */
 package lab_programacion2_semana2;
 
+import javax.swing.ImageIcon;
+import java.util.Calendar;
+
 /**
  *
  * @author mavasquez
  */
-public class EmpleadoVentas {
+public class EmpleadoVentas extends Empleado {
+    private double tasaComision;
+    private double ventasMensuales[];
     
+
+    public EmpleadoVentas(String codigo, String nombre, double salario, ImageIcon foto) {
+        super(codigo, nombre, salario, foto);
+        this.tasaComision = tasaComision;
+        ventasMensuales = new double [12];
+    }
+    public void registroDeVentas(double monto){
+         int mesActual = Calendar.getInstance().get(Calendar.MONTH);
+         double ventasAntes = ventasMensuales[mesActual];
+         double nuevasVentas = ventasAntes+ monto;
+         ventasMensuales[mesActual] = nuevasVentas;
+    }
+    
+    public double calculoDeComision(){
+        double comisionMes = 0;
+        int mes = Calendar.getInstance().get(Calendar.MONTH);
+        double ventas = ventasMensuales[mes];
+        if (ventas != 0) {
+            comisionMes = tasaComision * ventas;
+        }
+        return comisionMes;
+    }
+    
+    public double calcularPago() {
+    double pagoPorHoras = (horasTrabajadas / 160.0) * salarioBase;
+    double comisionMes = calculoDeComision();
+    double pagoFinal = pagoPorHoras + comisionMes;
+    
+    return pagoFinal;
+    }
 }
